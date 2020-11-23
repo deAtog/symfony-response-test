1999d399a98f32e9d385c45487da6593640225b3
David Ellingsworth <David.Ellingsworth@davey.com>
2020-11-23 17:46:50 -0500

    Add test cases for requesting an empty response.

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,73 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import $ from 'jquery';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/app.js');
+$(function(){
+    let fetchTypes = [
+        'text/plain',
+        'text/html',
+        'application/json'
+    ];
+
+    let ajaxTypes = [
+        'text',
+        'html',
+        'json',
+    ];
+
+    let body = $('body');
+
+    let report = function(method, type, data) {
+        let clazz = (data.length === 0)
+            ? 'success'
+            : 'error';
+
+        body.append(`<p class=${clazz}>Request via ${method} for ${type} returned content with a length: ${data.length}</p>`);
+    }
+
+    let getData = async function(headers) {
+        headers = headers || {};
+
+        let response = await fetch(
+            '/data', {
+                headers,
+            }
+        );
+
+        return response.text();
+    }
+
+    fetchTypes.forEach(type => {
+        let headers = {
+            'Content-Type':type,
+            'X-Requested-With':'XMLHttpRequest',
+        };
+
+        getData(headers).then(
+            data => report('fetch api with X-Requested-With', type, data)
+        );
+    });
+
+    fetchTypes.forEach(type => {
+        let headers = {
+            'Content-Type':type,
+        };
+
+        getData(headers).then(
+            data => report('fetch api', type, data)
+        );
+    });
+
+    ajaxTypes.forEach(dataType => {
+        $.ajax({
+            url:'/data?ajax',
+            dataType,
+        }).then(
+            (data, status, req) => report('$.ajax', dataType, data)
+        )
+    })
+});
